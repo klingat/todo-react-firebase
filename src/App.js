374e74b0d98fae2234ecdd542d4b4b52a4926f53
Kash Lingat <klingat@me.com>
2021-08-08 17:15:32 -0700

    Add lint rules, and firebase config

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+
+// import "firebase/auth"
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p>Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,11 +17,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+            Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
