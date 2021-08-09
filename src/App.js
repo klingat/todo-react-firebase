20836c013b61b53d471f0d730cfde3f3f232afa3
Kash Lingat <klingat@me.com>
2021-08-08 17:26:24 -0700

    Install & use styled components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
 import React from 'react'
 import logo from './logo.svg'
+import { ToDo } from './components/ToDo'
+import styled, { keyframes } from 'styled-components'
 
-// import "firebase/auth"
-import './App.css'
+const appLogoSpin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+const Container = styled.div`
+  text-align: center;
+`
+
+const AppHeader = styled.header`
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+    a {
+      color: #61dafb;
+    }
+`
+
+const AppLogo = styled.img`
+    height: 40vmin;
+    pointer-events: none;
+    animation: ${appLogoSpin} infinite 20s linear;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Container>
+      <AppHeader>
+        <AppLogo src={logo} alt="logo" />
         <p>Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -19,8 +51,9 @@ function App() {
         >
             Learn React
         </a>
-      </header>
-    </div>
+      </AppHeader>
+      <ToDo />
+    </Container>
   )
 }
 
--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const Todo = () => {
+export const ToDo = () => {
   return (
     <div> Hello todo</div>
   )
